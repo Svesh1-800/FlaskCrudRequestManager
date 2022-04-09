111952ed5c558098fc25c8b6d7194e521480334a
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-04-09 09:12:03 +0300

    Made header

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.index.html")
+    return render_template("index.html")
 
 
 if __name__=="__main__":
