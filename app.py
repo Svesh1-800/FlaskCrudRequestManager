dc5f34e2dcd7221ac43aa6b42dff93dbd1d68c29
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-04-14 17:31:59 +0300

    Connect with db

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask
 from flask import render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.secret_key = "Secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self,name,email,phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
 
 @app.route('/')
 def index():
