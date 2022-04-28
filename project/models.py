a8b71c4cf30f39f57a4dcc475a7b48988df89cfd
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-04-28 14:44:41 +0300

    new model User for authorization

--- project/models.py
+++ project/models.py
@@ -1,12 +1,35 @@
+import datetime
 from project import db
+from werkzeug.security import generate_password_hash,  check_password_hash
 
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(100))
-
+    #req_type_id = db.Column(db.Integer, db.ForeignKey('type_of_request.id'))
     def __init__(self,name,email,phone):
         self.name = name
         self.email = email
         self.phone = phone
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.datetime.now())
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username) 
+        
+    def set_password(self, password):
+	    self.password_hash = generate_password_hash(password)
+
+    def check_password(self,  password):
+	    return check_password_hash(self.password_hash, password)
+    
+""" class TypeOfRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    data_id = db.relationship('Data',backref='request') """
--- runner.py
+++ runner.py
@@ -1,13 +1,16 @@
 import os
 
+from flask_migrate import Migrate
 
 from project import create_app
 from project.views import crud_requests
+from project.database import db
 app = create_app()
+
 app.register_blueprint(crud_requests)
-    
+app.app_context().push()
 
-
+migrate = Migrate(app,db)
 
 if __name__ == '__main__':
     app.run()
