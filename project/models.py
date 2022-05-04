221177fddea26943fb0d2fd22a2f3689f521ddfe
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-05-04 20:04:52 +0300

    New model DataType
    - new model for type of data
    - new migrations and db

--- project/models.py
+++ project/models.py
@@ -7,10 +7,19 @@ class Data(db.Model):
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(100))
+    data_type_id = db.Column(db.Integer,db.ForeignKey('data-type.id'))
     def __init__(self,name,email,phone):
         self.name = name
         self.email = email
         self.phone = phone
+class DataType(db.Model):
+    __tablename__ = 'data-type'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+
+    def __repr__(self):
+        return '<DataType {}>'.format(self.name)
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
--- runner.py
+++ runner.py
@@ -1,29 +1,17 @@
-""" import os
-
-from flask_migrate import Migrate
-
-from project import create_app
-from project.views import crud_requests
-from project.database import db
-app = create_app()
-
-app.register_blueprint(crud_requests)
-app.app_context().push()
-
-migrate = Migrate(app,db,render_as_batch=True)
-
-if __name__ == '__main__':
-    app.run() """
 from project.database import db
 from flask import Flask
+from project.models import Data, DataType
 import os
 import config
 from project.views import table
+from flask_migrate import Migrate
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
     db.init_app(app)
+    migrate = Migrate(app,db)
     app.register_blueprint(table)
     return app
 def setup_database(app):
@@ -34,4 +22,6 @@ if __name__ == '__main__':
     app = create_app()
     if not os.path.isfile('/test.db'):
         setup_database(app)
+    
     app.run()
+
