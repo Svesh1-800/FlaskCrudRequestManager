ad65caf4647ff5c98c256ab2cd649390b04de141
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-05-24 19:25:19 +0300

    User registation form

---
+++ auth/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class UserSignUpForm(FlaskForm):
+    email = EmailField('email',render_kw={"placeholder": "Email"}, validators=[DataRequired(),Email()])
+    name = StringField('name',render_kw={"placeholder": "Username"}, validators=[DataRequired()])
+    password = PasswordField('password',render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=8,message='Password should be at least %(min)d characters long')])
--- auth/views.py
+++ auth/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from database import db
 from .models import User
 from flask_login import login_user, login_required, logout_user
-
+from .forms import UserSignUpForm
 auth = Blueprint('auth', __name__,template_folder='templates',static_folder='static',static_url_path='/auth/static')
 
 @auth.route('/login', methods=['POST','GET'])
@@ -19,23 +19,25 @@ def login():
 
 @auth.route('/signup', methods=['POST', 'GET'])
 def signup():
-    if request.method=="GET":
-        return render_template('auth/signup.html')
-    else:
-        data = request.form
-        email = data['email']
-        name = data['name']
-        password = data['password']
+    form = UserSignUpForm()
+    if form.validate_on_submit():
+        
+        email = form.email.data
+        name = form.name.data
+        password = form.password.data
         check_user = User.query.filter_by(email=email).first()
         if check_user:
-            flash("Такой чел уже есть")
+            
+            flash("Such email is already exsist")
             return redirect(url_for('auth.signup'))
         new_user = User(username=name,email=email)
         new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for("auth.login"))
-            
+        return redirect(url_for("table.index"))
+    return render_template('auth/signup.html',form= form)
+ 
+
 
 @auth.route('/logout')
 def logout():
