6633530b6ceba967885580821a015a4feaa522d3
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-07-10 10:06:16 +0300

    usersignINform

--- auth/forms.py
+++ auth/forms.py
@@ -7,4 +7,11 @@ class UserSignUpForm(FlaskForm):
     email = EmailField('email',render_kw={"placeholder": "Email"}, validators=[DataRequired(),Email()])
     name = StringField('name',render_kw={"placeholder": "Username"}, validators=[DataRequired()])
     password = PasswordField('password',render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=8,message='Password should be at least %(min)d characters long')])
+
+# not in use
+class UserSignInForm(FlaskForm):
+    email = EmailField('email',render_kw={"placeholder": "Email"}, validators=[DataRequired(),Email()])
+    password = PasswordField('password',render_kw={"placeholder": "Password"}, validators=[DataRequired(), Length(min=8,message='Password should be at least %(min)d characters long')])
+
+
   
