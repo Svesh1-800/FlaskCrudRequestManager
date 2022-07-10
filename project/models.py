d9d673c265709c2b21ebc052f343e74ae9ae6f10
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-07-10 10:12:07 +0300

    Update models with new model department

--- project/models.py
+++ project/models.py
@@ -10,6 +10,7 @@ class Data(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     data_type = db.Column(db.String(100))
     #data_type_id = db.Column(db.Integer,db.ForeignKey('data-type.id')) # это на будущее
+    #department_id = db.Column(db.Integer,db.ForeignKey('data-type.id')) # это на будущее
 
     def __init__(self,name,email,phone,req_type,user_id):
         self.name = name
@@ -18,7 +19,7 @@ class Data(db.Model):
         self.data_type = req_type
         self.user_id = user_id
 
-
+# not in use
 class DataType(db.Model):
     __tablename__ = 'data-type'
     id = db.Column(db.Integer, primary_key=True)
@@ -27,4 +28,18 @@ class DataType(db.Model):
 
     def __repr__(self):
         return '<DataType {}>'.format(self.name)
+    
+#not in use
+class Department(db.Model):
+    __tablename__='department'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(300))
+    active = db.Column(db.Boolean,default=True)
+
+    def __repr__(self):
+        return '<Department {}>'.format(self.name)
+
+    def __str__(self):
+        return 'Department: {}'.format(self.name)
 
